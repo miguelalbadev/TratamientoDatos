092e48fa5247873a7057b1f9adb557ff7c86c6c0
usuario <usuario@L-1706039>
2017-07-06 18:15:45 +0200

    Arreglo de la clase Datos

--- TratamientoDatos/Datos.cs
+++ TratamientoDatos/Datos.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace TratamientoDatos {
     public class Datos {
-
-        private string nombre;
-        private int primerNumero;
-        private int segundoNumero;
-
+                
         public int PrimerNumero { get; set; }
         public int SegundoNumero { get; set; }
         public string Nombre { get; set; }
